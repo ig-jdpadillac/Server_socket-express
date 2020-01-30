8b87ad779f928a6e6dc6aa1b648faa606748f94a
Jonathan <jonathan962911@gmail.com>
2020-01-30 12:56:55 -0500

    Primer commit  / Creando servidor en express

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const enviroment_1 = require("./global/enviroment");
+const server_1 = __importDefault(require("./classes/server"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const server = new server_1.default();
+// BodyParser
+server.app.use(body_parser_1.default.urlencoded({ extended: true }));
+server.app.use(body_parser_1.default.json());
+// CORS
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+server.start(() => {
+    console.log(`Servidor correndo en puerto ${enviroment_1.SERVER_PORT}`);
+});
